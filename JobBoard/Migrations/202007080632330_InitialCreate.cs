5f607453854becda717ccab30a5a7e1683827437
Luis Zelaya <luis_zelaya@hotmail.com>
2020-07-08 01:34:41 -0600

    Migration Changes

---
+++ JobBoard/Migrations/202007080632330_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace JobBoard.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Jobs",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Job_Title = c.String(),
+                        Job_Description = c.String(),
+                        Job_CreatedAt = c.DateTime(nullable: false),
+                        Job_ExpiresAt = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Jobs");
+        }
+    }
+}
