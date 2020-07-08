e92ba17e737afbf4788c14ede596c8b403d99c24
Luis Zelaya <luis_zelaya@hotmail.com>
2020-07-08 00:50:38 -0600

    Create Project

---
+++ JobBoard/Controllers/JobsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobBoard.Models
+{
+    public class JobsController : Controller
+    {
+        private JobsDBContext db = new JobsDBContext();
+
+        // GET: Jobs
+        public ActionResult Index()
+        {
+            return View(db.Job.ToList());
+        }
+
+        // GET: Jobs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Job.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // GET: Jobs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jobs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,Job_Title,Job_Description,Job_CreatedAt,Job_ExpiresAt")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Job.Add(jobs);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(jobs);
+        }
+
+        // GET: Jobs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Job.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // POST: Jobs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,Job_Title,Job_Description,Job_CreatedAt,Job_ExpiresAt")] Jobs jobs)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(jobs).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(jobs);
+        }
+
+        // GET: Jobs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Jobs jobs = db.Job.Find(id);
+            if (jobs == null)
+            {
+                return HttpNotFound();
+            }
+            return View(jobs);
+        }
+
+        // POST: Jobs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Jobs jobs = db.Job.Find(id);
+            db.Job.Remove(jobs);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ JobBoard/Models/Jobs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+
+namespace JobBoard.Models
+{
+    public class Jobs
+    {
+        [DisplayName("ID")]
+        public int id { get; set; }
+        [DisplayName("TITLE")]
+        public string Job_Title { get; set; }
+        [DisplayName("DESCRIPTION")]
+        public string Job_Description { get; set; }
+        [DisplayName("CREATED")]
+        public DateTime Job_CreatedAt { get; set; }
+        [DisplayName("EXPIRED")]
+        public DateTime Job_ExpiresAt { get; set; }
+        [DisplayName("COMMENTS")]
+        public string Job_Comments { get; set; }
+    }
+    public class JobsDBContext : DbContext
+    {
+        public DbSet<Jobs> Job { get; set; }
+
+    }
+}
